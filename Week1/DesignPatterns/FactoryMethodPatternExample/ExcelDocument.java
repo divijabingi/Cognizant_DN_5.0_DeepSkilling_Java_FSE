639240cf2b1048b95d76ed58988b5de98a5814f8
divijabingi <divijabingi@gmail.com>
2026-06-20 06:14:07 +0530

    Enhance ExcelDocument methods for better output
    Updated open method to provide more details and added close method.

--- Week1/DesignPatterns/FactoryMethodPatternExample/ExcelDocument.java
+++ Week1/DesignPatterns/FactoryMethodPatternExample/ExcelDocument.java
@@ -1,7 +1,11 @@
 public class ExcelDocument implements Document {
-
     @Override
     public void open() {
-        System.out.println("Opening Excel Document");
+        System.out.println(">> Excel Document is ready for data entry!");
+        System.out.println("   Format: .xlsx | App: MS Excel");
+    }
+    @Override
+    public void close() {
+        System.out.println("   Excel Document closed.\n");
     }
 }
