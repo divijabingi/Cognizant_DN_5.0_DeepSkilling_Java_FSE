b5db72e1640ce69114609625fb87a1c9a7a5d26e
divijabingi <divijabingi@gmail.com>
2026-06-20 06:14:43 +0530

    Fix formatting in ExcelDocument class

--- Week1/DesignPatterns/FactoryMethodPatternExample/ExcelDocument.java
+++ Week1/DesignPatterns/FactoryMethodPatternExample/ExcelDocument.java
@@ -1,7 +1,7 @@
 public class ExcelDocument implements Document {
     @Override
     public void open() {
-        System.out.println(">> Excel Document is ready for data entry!");
+        System.out.println(" Excel Document is ready for data entry!");
         System.out.println("   Format: .xlsx | App: MS Excel");
     }
     @Override
