ca0a4ee0db777cae419c6b7270f720f68302a764
divijabingi <divijabingi@gmail.com>
2026-06-20 06:15:40 +0530

    Enhance PdfDocument with open and close methods
    Updated the open method to provide more details and added a close method.

--- Week1/DesignPatterns/FactoryMethodPatternExample/PdfDocument.java
+++ Week1/DesignPatterns/FactoryMethodPatternExample/PdfDocument.java
@@ -1,7 +1,11 @@
 public class PdfDocument implements Document {
-
     @Override
     public void open() {
-        System.out.println("Opening PDF Document");
+        System.out.println(" PDF Document is ready to view!");
+        System.out.println("   Format: .pdf | App: PDF Reader");
+    }
+    @Override
+    public void close() {
+        System.out.println("   PDF Document closed.\n");
     }
 }
