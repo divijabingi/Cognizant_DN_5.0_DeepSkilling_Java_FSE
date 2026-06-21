05f233af43d8431c4e8cded3885cea963de37872
divijabingi <divijabingi@gmail.com>
2026-06-21 22:13:17 +0530

    Update Product.java

--- Week1/DSA/EcommercePlatformSearch/Product.java
+++ Week1/DSA/EcommercePlatformSearch/Product.java
@@ -1,9 +1,7 @@
 public class Product {
-
     int productId;
     String productName;
     String category;
-
     public Product(int productId, String productName, String category) {
         this.productId = productId;
         this.productName = productName;
