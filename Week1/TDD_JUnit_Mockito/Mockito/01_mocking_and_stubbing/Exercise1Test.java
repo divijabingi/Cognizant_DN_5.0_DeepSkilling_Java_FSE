77351dd008d794462541417748d05e0b43ba8e02
divijabingi <divijabingi@gmail.com>
2026-06-22 23:38:28 +0530

    Add files via upload

---
+++ Week1/TDD_JUnit_Mockito/Mockito/01_mocking_and_stubbing/Exercise1Test.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class Exercise1Test {
+
+    @Test
+    public void testMockingAndStubbing() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service = new MyService(mockApi);
+
+        String result = service.fetchData();
+
+        assertEquals("Mock Data", result);
+    }
+}
