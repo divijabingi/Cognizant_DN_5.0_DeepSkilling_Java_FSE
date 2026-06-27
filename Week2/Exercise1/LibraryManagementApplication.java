bc56463ebd312cfe75c6a1add104f4b112f49018
divijabingi <divijabingi@gmail.com>
2026-06-27 22:17:59 +0530

    Update LibraryManagementApplication.java

--- Week2/Exercise1/LibraryManagementApplication.java
+++ Week2/Exercise1/LibraryManagementApplication.java
@@ -7,18 +7,18 @@ import com.library.service.BookService;
 
 public class LibraryManagementApplication {
     public static void main(String[] args) {
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
-        BookService bookService =
-                context.getBean("bookService", BookService.class);
-        BookRepository bookRepository =
-                context.getBean("bookRepository", BookRepository.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookService = context.getBean("bookService", BookService.class);
+        BookRepository bookRepository = context.getBean("bookRepository", BookRepository.class);
+        
         bookService.addBook("Java Programming");
         bookRepository.save("Java Programming");
         System.out.println();
+        
         bookService.addBook("Database Systems");
         bookRepository.save("Database Systems");
         System.out.println();
+        
         bookService.listBooks();
         bookRepository.findAll();
     }
