5bbd502f1f5e12ad83a4f663c82e9de1b20f9f30
divijabingi <divijabingi@gmail.com>
2026-06-29 07:38:58 +0530

    Add files via upload

--- Week2/Spring_Core_Maven/ex2_dependency_injection/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week2/Spring_Core_Maven/ex2_dependency_injection/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,1 +1,20 @@
+package com.library.service;
+import com.library.repository.BookRepository;
 
+public class BookService {
+    private BookRepository bookRepository;
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String bookName) {
+        System.out.println("Book: " + bookName);
+        bookRepository.save(bookName);
+    }
+
+    public void listBooks() {
+         System.out.println("Accessing BookRepository through Dependency Injection.");
+        System.out.println("Displaying Library Books...");
+        bookRepository.findAll();
+    }
+}
