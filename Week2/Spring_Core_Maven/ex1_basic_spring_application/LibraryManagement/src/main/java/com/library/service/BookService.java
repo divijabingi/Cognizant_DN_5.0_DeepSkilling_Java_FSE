0fd70480c773cb5c48ea2f6040117cf46b9e478b
divijabingi <divijabingi@gmail.com>
2026-06-29 07:14:05 +0530

    Add files via upload

--- Week2/Spring_Core_Maven/ex1_basic_spring_application/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week2/Spring_Core_Maven/ex1_basic_spring_application/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,1 +1,10 @@
+package com.library.service;
 
+public class BookService {
+    public void addBook(String bookName) {
+        System.out.println("Adding Book: " + bookName);
+    }
+    public void listBooks() {
+        System.out.println("Displaying Book List");
+    }
+}
