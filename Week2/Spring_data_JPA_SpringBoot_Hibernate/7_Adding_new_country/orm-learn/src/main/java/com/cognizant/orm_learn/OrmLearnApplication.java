2b6baf629ad423de26bb7f9d252a3a62e13b5a70
divijabingi <divijabingi@gmail.com>
2026-06-29 23:55:22 +0530

    Add files via upload

--- Week2/Spring_data_JPA_SpringBoot_Hibernate/7_Adding_new_country/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
+++ Week2/Spring_data_JPA_SpringBoot_Hibernate/7_Adding_new_country/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -1,1 +1,34 @@
+package com.cognizant.orm_learn;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    @Override
+    public void run(String... args) {
+
+        Country country = new Country("TS", "Test Country");
+
+        countryService.addCountry(country);
+
+        System.out.println("Country added successfully.");
+
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OrmLearnApplication.class, args);
+
+    }
+
+}
