b791c2333ba87c7b0cdbf09cad9c0870cc751f65
divijabingi <divijabingi@gmail.com>
2026-06-29 19:25:49 +0530

    Add files via upload

---
+++ Week2/Spring_data_JPA_SpringBoot_Hibernate/ex1_springdataJPA_example/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,30 @@
+package com.cognizant.orm_learn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        System.out.println("Country List");
+
+        for (Country country : countryService.getAllCountries()) {
+            System.out.println(country.getCode() + " - " + country.getName());
+        }
+    }
+}
