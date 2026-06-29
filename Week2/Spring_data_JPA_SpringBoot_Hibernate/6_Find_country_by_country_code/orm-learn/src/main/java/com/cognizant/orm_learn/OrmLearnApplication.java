438bd70bf852be3c2c77987dd9d5fc20b1f2fcc3
divijabingi <divijabingi@gmail.com>
2026-06-30 00:01:44 +0530

    Update OrmLearnApplication.java

--- Week2/Spring_data_JPA_SpringBoot_Hibernate/6_Find_country_by_country_code/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
+++ Week2/Spring_data_JPA_SpringBoot_Hibernate/6_Find_country_by_country_code/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -7,14 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cognizant.orm_learn.model.Country;
 import com.cognizant.orm_learn.service.CountryService;
-import org.springframework.boot.Banner;
-
 
 @SpringBootApplication
 public class OrmLearnApplication implements CommandLineRunner {
 
     @Autowired
     private CountryService countryService;
+
     @Override
     public void run(String... args) {
 
@@ -26,12 +25,9 @@ public class OrmLearnApplication implements CommandLineRunner {
     }
 
     public static void main(String[] args) {
-    
-        SpringApplication app = new SpringApplication(OrmLearnApplication.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+
+        SpringApplication.run(OrmLearnApplication.class, args);
+
     }
 
-    
-    
 }
