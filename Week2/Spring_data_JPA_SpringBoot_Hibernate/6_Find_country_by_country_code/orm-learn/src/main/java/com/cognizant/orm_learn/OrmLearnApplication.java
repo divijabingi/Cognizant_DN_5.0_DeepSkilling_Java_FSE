b9bc9ad979039ebfa0bf554e96306ed614bc68b1
divijabingi <divijabingi@gmail.com>
2026-06-29 23:33:59 +0530

    Add files via upload

--- Week2/Spring_data_JPA_SpringBoot_Hibernate/6_Find_country_by_country_code/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
+++ Week2/Spring_data_JPA_SpringBoot_Hibernate/6_Find_country_by_country_code/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -1,1 +1,37 @@
+package com.cognizant.orm_learn;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+import org.springframework.boot.Banner;
+
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+    @Override
+    public void run(String... args) {
+
+        Country country = countryService.getCountry("IN");
+
+        System.out.println("Country Details");
+        System.out.println(country.getCode() + " - " + country.getName());
+
+    }
+
+    public static void main(String[] args) {
+    
+        SpringApplication app = new SpringApplication(OrmLearnApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+    
+    
+}
