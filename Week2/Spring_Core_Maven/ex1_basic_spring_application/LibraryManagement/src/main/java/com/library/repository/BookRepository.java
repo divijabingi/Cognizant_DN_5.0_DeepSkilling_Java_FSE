aa463563c7d022ff4f83288c14f42ab17b5d348a
divijabingi <divijabingi@gmail.com>
2026-06-29 07:12:45 +0530

    Add files via upload

--- Week2/Spring_Core_Maven/ex1_basic_spring_application/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++ Week2/Spring_Core_Maven/ex1_basic_spring_application/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -1,1 +1,19 @@
+package com.library.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookRepository {
+
+    private List<String> books = new ArrayList<>();
+    public void save(String bookName) {
+        books.add(bookName);
+        System.out.println("Book Added: " + bookName);
+    }
+    public void findAll() {
+        System.out.println("Books Available in the Library:");
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println((i + 1) + ". " + books.get(i));
+        }
+    }
+}
