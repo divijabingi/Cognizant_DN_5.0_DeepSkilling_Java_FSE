8f3b6d6b8cfe36d5bc8e9a01c47dcc75aad1986a
divijabingi <divijabingi@gmail.com>
2026-07-13 22:30:05 +0530

    Add files via upload

---
+++ Week5/React/Exercise_13.ReactJS/bloggerapp/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+
+import BookDetails from './BookDetails';
+import BlogDetails from './BlogDetails';
+import CourseDetails from './CourseDetails';
+
+import { books } from './Books';
+import { blogs } from './Blogs';
+import { courses } from './Courses';
+
+function App() {
+
+  return (
+
+    <div className="container">
+
+      <div className="column">
+
+        <h1>Course Details</h1>
+
+        <CourseDetails courses={courses} />
+
+      </div>
+
+      <div className="column">
+
+        <h1>Book Details</h1>
+
+        <BookDetails books={books} />
+
+      </div>
+
+      <div className="column">
+
+        <h1>Blog Details</h1>
+
+        <BlogDetails blogs={blogs} />
+
+      </div>
+
+    </div>
+
+  );
+
+}
+
+export default App;
---
+++ Week5/React/Exercise_13.ReactJS/bloggerapp/src/Blogs.js
@@ -0,0 +1,17 @@
+export const blogs = [
+
+    {
+        id: 1,
+        title: "React Learning",
+        author: "Stephen Biz",
+        description: "Welcome to learning React!"
+    },
+
+    {
+        id: 2,
+        title: "Installation",
+        author: "Schwzenedier",
+        description: "You can install React from npm."
+    }
+
+];
